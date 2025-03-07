fd41b5d6b40396aa7494019f3fee18b1a31ccb4c
揚恩 張 <anna0983266723@gmail.com>
2025-03-07 15:30:39 +0800

    1

--- SQL_twolist/Form1.Designer.cs
+++ SQL_twolist/Form1.Designer.cs
@@ -53,7 +53,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(341, 12);
+            this.button1.Location = new System.Drawing.Point(341, 35);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(83, 36);
             this.button1.TabIndex = 1;
@@ -65,7 +65,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label3.Location = new System.Drawing.Point(27, 29);
+            this.label3.Location = new System.Drawing.Point(27, 52);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(71, 16);
             this.label3.TabIndex = 13;
@@ -76,7 +76,7 @@
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panel1.Controls.Add(this.radioButton2);
             this.panel1.Controls.Add(this.radioButton1);
-            this.panel1.Location = new System.Drawing.Point(118, 20);
+            this.panel1.Location = new System.Drawing.Point(118, 43);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(160, 34);
             this.panel1.TabIndex = 14;
@@ -104,7 +104,7 @@
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(118, 88);
+            this.numericUpDown1.Location = new System.Drawing.Point(118, 111);
             this.numericUpDown1.Name = "numericUpDown1";
             this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
             this.numericUpDown1.TabIndex = 15;
@@ -118,7 +118,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label1.Location = new System.Drawing.Point(27, 94);
+            this.label1.Location = new System.Drawing.Point(27, 117);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(71, 16);
             this.label1.TabIndex = 16;
@@ -126,7 +126,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(341, 74);
+            this.button2.Location = new System.Drawing.Point(341, 97);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(83, 36);
             this.button2.TabIndex = 17;
