94991b4ec056642335d9d275622ba226bb042b52
揚恩 張 <anna0983266723@gmail.com>
2025-03-07 12:11:26 +0800

    加入專案檔案。

---
+++ SQL_twolist/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace SQL_twolist
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(30, 228);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(394, 225);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(341, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 36);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "開啟資料";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(27, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 16);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "選擇資料";
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.radioButton2);
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Location = new System.Drawing.Point(118, 20);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(160, 34);
+            this.panel1.TabIndex = 14;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 7);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(54, 16);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.Text = "Orders";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(81, 7);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(48, 16);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "Users";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(118, 88);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown1.TabIndex = 15;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(27, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 16);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "使用者ID";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(341, 74);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(83, 36);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "查詢";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(470, 480);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ SQL_twolist/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace SQL_twolist
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static string connectionstring = "Server=anna\\SQLEXPRESS;Database=test2;Trusted_Connection=True;";
+        public static string query;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                query = "SELECT * FROM [dbo].[Orders]";
+                using (SqlConnection conn = new SqlConnection(connectionstring))
+                {
+                    SqlCommand comm = new SqlCommand(query, conn);
+                    conn.Open();
+                    using (SqlDataReader reader = comm.ExecuteReader())
+                    {
+                        dataGridView1.Rows.Clear();
+                        dataGridView1.Columns.Clear();
+                        dataGridView1.Columns.Add("OrderID", "OrderID");
+                        dataGridView1.Columns.Add("UserID", "UserID");
+                        dataGridView1.Columns.Add("Product", "Product");
+                        while (reader.Read())
+                        {
+                            dataGridView1.Rows.Add(reader["OrderID"], reader["UserID"], reader["Product"]);
+                        }
+                    }
+                }
+            }
+            else if(radioButton2.Checked)
+            {
+                query = "SELECT * FROM [dbo].[Users]";
+                using (SqlConnection conn = new SqlConnection(connectionstring))
+                {
+                    SqlCommand comm = new SqlCommand(query, conn);
+                    conn.Open();
+                    using (SqlDataReader reader = comm.ExecuteReader())
+                    {
+                        dataGridView1.Rows.Clear();
+                        dataGridView1.Columns.Clear();
+                        dataGridView1.Columns.Add("UserID", "UserID");
+                        dataGridView1.Columns.Add("Name", "Name");
+                        while (reader.Read())
+                        {
+                            dataGridView1.Rows.Add(reader["UserID"], reader["Name"]);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //using(SqlConnection conn = new SqlConnection(connectionstring))
+            //{
+            //    try
+            //    {
+            //        conn.Open();
+            //        MessageBox.Show("ok");
+            //    }
+            //    catch
+            //    {
+            //        MessageBox.Show("error");
+            //    }
+            //}
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            query = "SELECT [Orders].[OrderID],[Users].[Name],[Orders].[Product] FROM [dbo].[Users] JOIN [dbo].[Orders] ON [Orders].[UserID] = @UserID AND [Users].[UserID] = @UserID;";
+            using(SqlConnection conn = new SqlConnection(connectionstring))
+            {
+                SqlCommand comm = new SqlCommand(query, conn);
+                comm.Parameters.AddWithValue("@UserID", numericUpDown1.Value);
+
+                conn.Open();
+                comm.ExecuteNonQuery();
+                using(SqlDataReader reader = comm.ExecuteReader())
+                {
+                    dataGridView1.Rows.Clear();
+                    dataGridView1.Columns.Clear();
+                    dataGridView1.Columns.Add("OrderID", "OrderID");
+                    dataGridView1.Columns.Add("Name", "Name");
+                    dataGridView1.Columns.Add("Product", "Product");
+
+                    while (reader.Read())
+                    {
+                        dataGridView1.Rows.Add(reader["OrderID"], reader["Name"], reader["Product"]);
+                    }
+                }
+            }
+        }
+    }
+}
